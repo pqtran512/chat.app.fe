fa918ab5999e49c4109fabd6c958f5df10de847e
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2024-09-30 11:31:29 +0700

    update login ui

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,12 +1,22 @@
+import { Box } from "@mui/material";
 import { useEffect, useState, FC } from "react";
 
 interface LoginProps {}
 
 const Login: FC<LoginProps> = ({}) => {
   return (
-    <>
-      <p>Login page</p>
-    </>
+    <Box
+      sx={{
+        height: 300,
+        width: 400,
+        backgroundColor: "#fff",
+        padding: "30px 45px",
+        borderRadius: "2px",
+        position: 'relative',
+        margin: '0 auto',
+        boxShadow: '0 8px 24px rgba(21, 48, 142, 0.14)',
+      }}
+    ></Box>
   );
 };
 
