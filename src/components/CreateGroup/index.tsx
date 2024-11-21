c4487c2fa1eaf1b41520b0b5237fb1d94b71a722
Dang Van Minh <minhdv0201@gmail.com>
2024-11-22 00:17:28 +0700

    add api for search friend

--- src/components/CreateGroup/index.tsx
+++ src/components/CreateGroup/index.tsx
@@ -50,6 +50,7 @@ const CreateGroupForm = ({ handleClose }) => {
 
   const handleCreateGroup = () => {
     createGroup.mutate(groupInfo);
+    handleClose();
   };
 
   const createGroup = useMutation(groupAPI.createGroup, {
